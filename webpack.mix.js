60467064ccea5aefef91bb8469c0b520d4d33887
Nicolas Vera <nicolasalevera98@gmail.com>
2021-05-15 21:16:51 -0300

    build: Add BrowserSync to mix
    With this module we are able to watch for changes of the app. This includes css, js and blade files.

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,3 +16,10 @@ mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'pub
     require('tailwindcss'),
     require('autoprefixer'),
 ]);
+
+mix.browserSync({
+    proxy: 'http://localhost:8000/',
+    files: [
+        "resources/**/*.blade.php",
+    ]
+})
