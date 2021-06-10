c04f95ae836bff2ed8c9d17fafcea54b20cd130f
Nicolas Alejandro Vera <nicolasalevera98@gmail.com>
2021-06-10 01:27:48 -0300

    fix: Modify back-header component to be an anchor

--- resources/views/components/back-header.blade.php
+++ resources/views/components/back-header.blade.php
@@ -1,6 +1,10 @@
+@props(['href' => '#'])
+
 <div {{$attributes->merge(['class' => "w-full"])}}>
-    <x-svg-o-arrow-left class="w-6 h-6 text-red-400 inline-block"/>
-    @isset($slot)
-        <h1 class="inline-block align-middle ml-2 text-xl">{{$slot}}</h1>
-    @endisset
+    <a href="{{$href}}" class="inline-block">
+        <x-svg-o-arrow-left class="w-6 h-6 text-red-400 inline-block"/>
+        @isset($slot)
+            <h1 class="inline-block align-middle ml-2 text-xl">{{$slot}}</h1>
+        @endisset
+    </a>
 </div>
